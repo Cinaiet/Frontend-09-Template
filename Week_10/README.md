62db9010bbae4e74886c61d99e3dcfc37f844860
王冠伟 <crazyguanwei@163.com>
2021-05-10 00:04:26 +0800

    Week10

---
+++ Week_10/README.md
@@ -0,0 +1,18 @@
+
+## 排版
+
+### flex排版
+
+### 主轴
+表示页面排版时的排列的方向
+
+```
+flex-direction: row //  Main主轴: contains  x, width , left , right
+                    //  Cross: y, height, top, bottom
+
+flex-direction: column //  Cross 交叉轴: contains  x, width , left , right
+                       //  Main: y, height, top, bottom
+
+
+
+```
---
+++ Week_10/server.js
@@ -0,0 +1,46 @@
+const http = require('http')
+
+http.createServer((req, res) => {
+  let body = []
+  req.on('error', (err) => {
+    console.log('err', err)
+  }).on('data', (chunk) => {
+    body.push(chunk.toString())
+  }).on('end', () => {
+    body =  (Buffer.concat([ Buffer.from(body.toString()) ])).toString()
+    console.log("body", body)
+    res.writeHead(200, { 'Content-Type': 'text/html'})
+    res.end(
+`<html lang="en">
+<head>
+</head>
+<style>
+#box {
+  background-color: rgb(255, 255, 255);
+  width: 500px;
+  height: 300px;
+  background-color: rgb(255, 255, 255);
+  display: flex;
+}
+#myid {
+  background-color: rgb(255, 0, 0);
+  width: 200px;
+  height: 100px;
+}
+.c1 {
+  background-color: rgb(0, 255, 0);
+  flex: 1;
+}
+</style>
+<body>
+<div id="box">
+  <div id="myid"></div>
+  <div class="c1"></div>
+</div>
+</body>
+</html>`
+    )
+  })
+}).listen(8088)
+
+console.log('server started')
